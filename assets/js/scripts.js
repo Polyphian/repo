5112178ac9aaf554d1d720752478e757d8405a33
Polyphian <48692915+Polyphian@users.noreply.github.com>
2020-10-15 03:42:34 -0400

    DepthsLite

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+function sticktothetop() {
+    var window_top = $(window).scrollTop();
+    var top = $('#stick-here').offset().top;
+    if (window_top > top) {
+        $('#stickThis').addClass('stick');
+        $('#stick-here').height($('#stickThis').outerHeight());
+    } else {
+        $('#stickThis').removeClass('stick');
+        $('#stick-here').height(0);
+    }
+}
+$(function() {
+    $(window).scroll(sticktothetop);
+    sticktothetop();
+});
