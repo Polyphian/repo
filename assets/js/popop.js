e3aca033dada21a3d35b17fb6819448c4f56e0da
Polyphian <48692915+Polyphian@users.noreply.github.com>
2021-01-02 06:33:53 -0500

    preview

---
+++ assets/js/popop.js
@@ -0,0 +1,8 @@
+function popop() {
+  var x = document.getElementById("popDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
