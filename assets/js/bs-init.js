ed2dbfc2ff124dedfab513134f7981662a366176
Polyphian <48692915+Polyphian@users.noreply.github.com>
2021-06-26 21:18:16 -0400

    release

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,3 +1,7 @@
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate');
+}
+
 $(document).ready(function(){
 	AOS.init();
 });
